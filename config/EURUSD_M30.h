b52bf3818490d29e0549d47890bb8a08e7c3f046
kenorb <kenorb@users.noreply.github.com>
2021-01-19 22:36:53 +0000

    Optimize Indicator params (pf=0.61; dd=48.92; tt=48145)

--- config/EURUSD_M30.h
+++ config/EURUSD_M30.h
@@ -1,15 +1,14 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
-struct Indi_MA_Params_M30 : MAParams {
-  Indi_MA_Params_M30() : MAParams(indi_ma_defaults, PERIOD_M30) {
-    period = 20;
-    ma_shift = 0;
-    ma_method = (ENUM_MA_METHOD)0;
+struct Indi_MA_Params_M30 : Indi_MA_Params {
+  Indi_MA_Params_M30() : Indi_MA_Params(indi_ma_defaults, PERIOD_M30) {
     applied_price = (ENUM_APPLIED_PRICE)0;
+    ma_method = (ENUM_MA_METHOD)0;
+    period = 2;
     shift = 0;
   }
 } indi_ma_m30;
@@ -21,12 +20,12 @@ struct Stg_MA_Params_M30 : StgParams {
     lot_size = 0;
     signal_open_method = 0;
     signal_open_filter = 1;
-    signal_open_level = (float)1;
+    signal_open_level = (float)1.0;
     signal_open_boost = 0;
     signal_close_method = 0;
-    signal_close_level = (float)1;
+    signal_close_level = (float)0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)1;
     tick_filter_method = 1;
     max_spread = 0;
   }
